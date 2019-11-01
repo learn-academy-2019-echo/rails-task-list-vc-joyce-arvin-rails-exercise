237045afb67aee188e83447c13396132cbb56fd6
ArvinLleva <arvin.lleva@yahoo.com>
2019-10-31 22:09:16 -0700

    figured out how to get the tasks to be displayed from a specific tasklist

--- app/controllers/tasklist_controller.rb
+++ app/controllers/tasklist_controller.rb
@@ -5,10 +5,23 @@ class TasklistController < ApplicationController
     end
 
     def tasklist_details
-        @id = params[:id].to_i
-        @tasklist_detail = TaskList.find_by(id: @id)
-        @title = @tasklist_detail.title
-        @description = @tasklist_detail.description
+        @tasklist_id = params[:id].to_i
+        @tasklist = TaskList.find_by(id: @tasklist_id)
+        @title = @tasklist.title
+        @description = @tasklist.description
+        @tasks = @tasklist.tasks
         render "tasklist_details.html.erb"
+    end
+
+    def task_details
+        @tasklist_id = params[:task_list_id].to_i
+        @id = params[:id].to_i
+        @tasklist = TaskList.find_by_id (@tasklist_id) 
+        @task = @tasklist.tasks.find_by_id(@id) 
+        @title = @task.title
+        @description = @task.description
+        @date_time_create = @task.created_at
+
+        render "task_detail.html.erb"
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/display-tasklist' => 'tasklist#display_tasklist'
   get '/tasklist-details/:id' => 'tasklist#tasklist_details'
+  get '/task-details/:id/:task_list_id' => 'tasklist#task_details'
+
+  root to: 'tasklist#display_tasklist'
 end
