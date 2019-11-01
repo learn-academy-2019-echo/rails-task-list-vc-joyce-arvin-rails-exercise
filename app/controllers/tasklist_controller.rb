2d36d81992e59e1fd7e0319634d7ee9edf5bae23
ArvinLleva <arvin.lleva@yahoo.com>
2019-10-31 17:01:58 -0700

    what we have after class, figured out getting a single tasklist

--- app/controllers/tasklist_controller.rb
+++ app/controllers/tasklist_controller.rb
@@ -1,7 +1,14 @@
 class TasklistController < ApplicationController
     def display_tasklist
-        @list = TaskList.all
-
+        @task_lists = TaskList.all
+        @task_list_third = TaskList.find_by(title: "TaskList 2")
         render "tasklist.html.erb"
+    end
+
+    def tasklist_details
+        @id = params[:id].to_i
+        @tasklist_detail = TaskList.find @id
+        @title = @tasklist_detail.title
+        render "tasklist_details.html.erb"
     end
 end
